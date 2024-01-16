e450271e88c56b303faa5d3c1cae732459803002
Jiwon <you@example.com>
2024-01-16 14:31:18 +0900

    [feat] presigned-url 기능 완성

--- src/main/java/com/example/AA/controller/FileUploadController.java
+++ src/main/java/com/example/AA/controller/FileUploadController.java
@@ -1,45 +1,55 @@
 package com.example.AA.controller;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Value;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-
+import com.example.AA.global.s3.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
-import lombok.RequiredArgsConstructor;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/file")
 @RequiredArgsConstructor
 public class FileUploadController {
 
-	private final AmazonS3Client amazonS3Client;
+	private final S3Service s3service;
 
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
-	@PostMapping
-	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+	@PostMapping("/upload")
+	public ResponseEntity<String> uploadFile(HttpServletRequest httpRequest, @RequestParam("file") MultipartFile file) {
 		try {
-			String fileName=file.getOriginalFilename();
-			String fileUrl= "https://" + bucket + "/test" +fileName;
-			ObjectMetadata metadata= new ObjectMetadata();
+			String fileName = file.getOriginalFilename();
+
+			// S3 Presigned URL 및 objectKey 생성
+			Map<String, Serializable> presignedUrlInfo = s3service.getPreSignedUrl(httpRequest,fileName);
+			String preSignedUrl = removeQueryString(presignedUrlInfo.get("preSignedUrl").toString());
+			String objectKey = presignedUrlInfo.get("objectKey").toString();
+
+			ObjectMetadata metadata = new ObjectMetadata();
 			metadata.setContentType(file.getContentType());
 			metadata.setContentLength(file.getSize());
-			amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
-			return ResponseEntity.ok(fileUrl);
+
+			// S3에 파일 업로드
+			s3service.uploadFileToS3(objectKey, file, metadata);
+
+			return ResponseEntity.ok(preSignedUrl);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
+	}
+
+	private String removeQueryString(String url) {
+		int index = url.indexOf("?");
+		return (index == -1) ? url : url.substring(0, index);
 	}
 }
