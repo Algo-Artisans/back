1da31e0d76fd1435fe2e377dcdc7efe51554fe5a
Jiwon <you@example.com>
2024-01-30 16:31:04 +0900

    [feat] controller 및 service 기본 로직 작성

--- src/main/java/com/example/AA/controller/PortfolioController.java
+++ src/main/java/com/example/AA/controller/PortfolioController.java
@@ -3,22 +3,73 @@ package com.example.AA.controller;
 import com.example.AA.dto.PortfolioReqDto;
 import com.example.AA.dto.PortfolioResDto;
 import com.example.AA.global.jwt.OAuthUserService;
+import com.example.AA.service.PortfolioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class PortfolioController {
 
     private final OAuthUserService oAuthUserService;
-    @PostMapping("/portfolio")
-    public PortfolioResDto postPortfolio(HttpServletRequest httpRequest, @RequestBody PortfolioReqDto portfolioReqDto) {
-        return oAuthUserService.createPortfolio(httpRequest, portfolioReqDto);
+    private final PortfolioService portfolioService;
+
+    // 내 포트폴리오 등록
+    @PostMapping("/myPortfolio")
+    public ResponseEntity<PortfolioResDto> postPortfolio(HttpServletRequest httpRequest, @RequestBody PortfolioReqDto portfolioReqDto) {
+         return ResponseEntity.ok(oAuthUserService.createPortfolio(httpRequest, portfolioReqDto));
     }
 
+    // 내 포트폴리오 조회
+    @GetMapping("/myPortfolio")
+    public ResponseEntity<PortfolioResDto> getPortfolio(HttpServletRequest httpRequest) {
+        return ResponseEntity.ok(portfolioService.getPortfolio(httpRequest));
+    }
 
+    // 디자이너 포트폴리오 전체 조회
+    @GetMapping("/portfolios")
+    public ResponseEntity<List<PortfolioResDto>> getPortfolios(){
+        List<PortfolioResDto> portfolioListResDto = portfolioService.getPortfolios;
+        return ResponseEntity.ok(portfolioListResDto);
+    }
+
+    // 디자이너 포트폴리오 키워드 필터링
+    @GetMapping("/search/portfilios")
+    public ResponseEntity<PortfolioSearchResDto> searchPortfolio(HttpServletRequest httpRequest,@RequestParam(name = "hairstyle") String s){
+        return ResponseEntity.ok(portfolioService.searchPortfolio(httpRequest,s));
+    }
+
+    // 디자이너 포트폴리오 드롭다운 조회
+    @GetMapping("/dropdown/portfilios")
+    public ResponseEntity<PortfolioDropDownResDto> dropdownPortfolio(HttpServletRequest httpRequest,@RequestParam(name = "dropdown") String s){
+        return ResponseEntity.ok(portfolioService.dropdownPortfolio(httpRequest,s));
+    }
+
+    // 내 포트폴리오 삭제
+    @DeleteMapping("/myPortfolio")
+    public ResponseEntity<?> deletePortfolio(HttpServletRequest httpRequest){
+        return ResponseEntity.ok(portfolioService.deletePortfolio(httpRequest));
+    }
+
+    // 내가 받은 좋아요 조회
+    @GetMapping("/myPortfolioLikes")
+    public ResponseEntity<PortfolioLikesResDto> getPortfolioLikes(HttpServletRequest httpRequest){
+        return ResponseEntity.ok(portfolioService.getPortfolioLikes(httpRequest));
+    }
+
+    // 내 광고 등록
+    @PostMapping("/myAdvertisement")
+    public ResponseEntity<AdvertisementResDto> postAdvertisement(HttpServletRequest httpRequest, @RequestBody AdvertisementReqDto advertisementReqDto) {
+        return ResponseEntity.ok(portfolioService.createAdvertisement(httpRequest, advertisementReqDto));
+    }
+
+    @DeleteMapping("/myAdvertisement")
+    public ResponseEntity<?> postAdvertisement(HttpServletRequest httpRequest) {
+        return ResponseEntity.ok(portfolioService.deleteAdvertisement(httpRequest));
+    }
 }
--- src/main/java/com/example/AA/entity/enumtype/Role.java
+++ src/main/java/com/example/AA/entity/enumtype/Role.java
@@ -9,7 +9,8 @@ public enum Role {
 
     USER("ROLE_USER", "유저"),
     GUEST("ROLE_GUEST", "손님"),
-    DESIGNER("ROLE_DESIGNER", "디자이너");
+    DESIGNER("ROLE_DESIGNER", "디자이너"),
+    ADMINISTRATOR("ROLE_ADMINISTRATOR","관리자");
 
     private final String key;
     private final String title;
