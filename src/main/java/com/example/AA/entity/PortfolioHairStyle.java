bd6b2298f2cd7f8044a405250dc3976b2d3ebc3e
Jiwon <you@example.com>
2024-02-10 23:36:23 +0900

    [feat] searchPortfolio service 로직 완성

--- src/main/java/com/example/AA/entity/PortfolioHairStyle.java
+++ src/main/java/com/example/AA/entity/PortfolioHairStyle.java
@@ -21,11 +21,11 @@ public class PortfolioHairStyle {
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "portfolio_id",nullable = true)
-    private Portfolio portfolio;
+    private Portfolio portfolio; // jiwon id1
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "hair_style_id",nullable = true)
-    private HairStyle hairStyle;
+    private HairStyle hairStyle; // 스트레이트, c컬단발, 보브컷
 
 
     public PortfolioHairStyle(Portfolio portfolio, HairStyle hairStyle) {
