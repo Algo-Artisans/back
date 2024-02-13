ab181c8cb6b30bef8fdbf70e00d024b3288c27c0
SeojungH <chseojung@naver.com>
2024-02-13 22:44:18 +0900

    [fix] search 수정 - 미완

--- src/main/java/com/example/AA/repository/PortfolioSearchRepository.java
+++ src/main/java/com/example/AA/repository/PortfolioSearchRepository.java
@@ -7,7 +7,10 @@ import com.example.AA.entity.*;
 import com.example.AA.entity.enumtype.HairName;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.ListPath;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -37,9 +40,14 @@ public class PortfolioSearchRepository {
         QPortfolio portfolio = QPortfolio.portfolio;
         QPortfolioHairStyle portfolioHairStyle = QPortfolioHairStyle.portfolioHairStyle;
         QWorkImage workImage = QWorkImage.workImage;
+        // QHairStyle hairStyle1 = QHairStyle.hairStyle;
+        // QHairStyle hairStyle2 = QHairStyle.hairStyle;
+        // QHairStyle hairStyle3 = QHairStyle.hairStyle;
+
         QHairStyle hairStyle1 = new QHairStyle("hairStyle1");
         QHairStyle hairStyle2 = new QHairStyle("hairStyle2");
         QHairStyle hairStyle3 = new QHairStyle("hairStyle3");
+
         log.info("searchHairname2");
         Predicate predicate = null;
         if (!StringUtils.isEmpty(hairName)) {
@@ -47,23 +55,24 @@ public class PortfolioSearchRepository {
             predicate = portfolioHairStyle.hairStyle.hairName.eq(convertedHairName);
         }
         log.info(predicate.toString());
-        return queryFactory
-                .selectDistinct(new QPortfolioResDto(
-                        portfolio.user,
-                        portfolio,
-                        workImage,
-                        hairStyle1,
-                        hairStyle2,
-                        hairStyle3
-                ))
-                .from(portfolio)
-                .leftJoin(portfolio.portfolioHairStyles, portfolioHairStyle)
-                .leftJoin(portfolio.workImages, workImage)
-                .leftJoin(portfolioHairStyle.hairStyle, hairStyle1).on(hairStyle1.hairStyleId.eq(portfolioHairStyle.hairStyle.hairStyleId))
-                .leftJoin(portfolioHairStyle.hairStyle, hairStyle2).on(hairStyle2.hairStyleId.ne(hairStyle1.hairStyleId))
-                .leftJoin(portfolioHairStyle.hairStyle, hairStyle3).on(hairStyle3.hairStyleId.ne(hairStyle1.hairStyleId), hairStyle3.hairStyleId.ne(hairStyle2.hairStyleId))
-                .where(predicate)
-                .fetch();
+
+        return new JPAQueryFactory(em)
+            .select(Projections.constructor(PortfolioResDto.class,
+                portfolio.user,
+                portfolio,
+                workImage,
+                hairStyle1,
+                hairStyle2,
+                hairStyle3
+            ))
+            .from(portfolio)
+            .leftJoin(portfolio.portfolioHairStyles, portfolioHairStyle)
+            .leftJoin(portfolio.workImages, workImage)
+            .leftJoin(portfolioHairStyle.hairStyle, hairStyle1) //.on(hairStyle1.hairStyleId.eq(portfolioHairStyle.hairStyle.hairStyleId))
+            .leftJoin(portfolioHairStyle.hairStyle, hairStyle2) //.on(hairStyle2.hairStyleId.ne(hairStyle1.hairStyleId))
+            .leftJoin(portfolioHairStyle.hairStyle, hairStyle3) //.on(hairStyle3.hairStyleId.ne(hairStyle1.hairStyleId), hairStyle3.hairStyleId.ne(hairStyle2.hairStyleId))
+            .where(predicate)
+            .fetch();
     }
 
 }
